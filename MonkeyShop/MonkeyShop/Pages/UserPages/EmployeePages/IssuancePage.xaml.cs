4ea8c8dc5c8e4ffe1ff325ad5f54bd6f0473f29b
Ruslana <garaeva-r_23@mail.ru>
2023-10-24 01:39:26 +0300

    Adding IssuancePage design

--- MonkeyShop/MonkeyShop/Pages/UserPages/EmployeePages/IssuancePage.xaml.cs
+++ MonkeyShop/MonkeyShop/Pages/UserPages/EmployeePages/IssuancePage.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -23,6 +25,33 @@ namespace MonkeyShop.Pages.UserPages.EmployeePages
         public IssuancePage()
         {
             InitializeComponent();
+        }
+
+        private void CreateQRCode_Click(object sender, RoutedEventArgs e)
+        {
+            string soucer_xl = "https://www.ozon.ru/search/?from_global=true&text="; 
+            //https://www.ozon.ru/search/?from_global=true&text=саше+лаванды+и+хвои
+            QRCoder.QRCodeGenerator qr = new QRCoder.QRCodeGenerator();
+            QRCoder.QRCodeData data = qr.CreateQrCode(soucer_xl, QRCoder.QRCodeGenerator.ECCLevel.L);
+            QRCoder.QRCode code = new QRCoder.QRCode(data);
+            Bitmap bitmap = code.GetGraphic(100);
+
+            using (MemoryStream memory = new MemoryStream())
+            {
+                bitmap.Save(memory, System.Drawing.Imaging.ImageFormat.Bmp);
+                memory.Position = 0;
+                BitmapImage bitmapimage = new BitmapImage();
+                bitmapimage.BeginInit();
+                bitmapimage.StreamSource = memory;
+                bitmapimage.CacheOption = BitmapCacheOption.OnLoad;
+                bitmapimage.EndInit();
+                imageQr.Source = bitmapimage;
+            }
+        }
+
+        private void GetOrder_Click(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
